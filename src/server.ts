c019ad20e467a32fca2e6b96e30790073e45d63c
Paulo Matos <contato.paulomatos@gmail.com>
2023-06-19 20:03:33 -0300

    Added memories entities/routes, cors and migration

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true, // all front-end URLs can access our back-end
+  // TO-DO
+  // origin: ['http://localhost:3000'] // Recommended to add production urls
 })
+
+app.register(memoriesRoutes)
 
 app
   .listen({

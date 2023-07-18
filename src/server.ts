10f73150fb8d528109f753cf6e0e9c265c1c039a
Paulo Matos <contato.paulomatos@gmail.com>
2023-07-18 16:28:37 -0300

    Added Features: Upload files to disk, access to uploaded files with file URL

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import 'dotenv/config'
 
 import fastify from 'fastify'
-import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
-import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
+
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { memoriesRoutes } from './routes/memories'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // all front-end URLs can access our back-end
@@ -19,11 +30,13 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('✈  HTTP server running on http://localhost:3333')
